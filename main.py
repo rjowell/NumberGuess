fede3f03df78690ece36922545ec9c6f82735e3d
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-02-23 01:16:27 +0000

    stuff

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+
 import random
 # Random Number Guesser Game
 # Pick a random number within a range and have the user try to guess it.
@@ -6,17 +9,25 @@ the_number = random.randint(1,200)
 
 print("Welcome to the Random Number Guess Game")
 print("The number is between 1 and 200")
-user_guess = input("Guess the number ")
-
+user_guess = 0
+lives = 3
 while user_guess != the_number:
   user_guess = input("Guess the number ")
-  if user_guess.isalpha():
+  if user_guess.isalpha() == False:
     user_guess = int(user_guess)
     if user_guess < the_number:
         print("It's too low")
+        
+        lives -= 1
+        if lives == 0:
+          print("GaMe OvEr!")
+        print("You have")
     if user_guess > the_number:
         print("It's too high")
-      
+        lives -= 1
+        if lives == 0:
+          print("GaMe OvEr!")
+    
   else:
     print("That's not a number, please try again!")
     user_guess = input("Guess the number ")
@@ -30,10 +41,7 @@ over = False
 
 
 
-print("You Win")
+print("You Win a new luxury car!")
     
-
-
-
 
 
